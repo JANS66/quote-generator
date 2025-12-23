b1b5ace990c0b4771a147d391bbda0e46221eff1
JANS66 <98973687+JANS66@users.noreply.github.com>
2025-12-23 15:45:58 +0200

    Set up state in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
+import React, { useEffect, useState } from "react"
+import QuoteBox from "./QuoteBox.jsx"
+import CategorySelector from "./CategorySelector.jsx"
+
 function App() {
+  const [quote, setQuote] = useState({})
+  const [category, setCategory] = useState(`inspirational`)
+
+  const fetchQuote = async (category) => {
+    const response = await fetch(`https://api.quotable.io/random?tags=${category}`)
+    const data = await response.json()
+    setQuote(data)
+  }
+
+  useEffect(() => {
+    fetchQuote(category)
+  }, [category]) 
+
   return (
-    <>
-    </>
+    <div className="App">
+      <CategorySelector category={category} setCategory={setCategory} />
+      <QuoteBox quote={quote}/>
+      <button onClick={() => fetchQuote(category)}>New Quote</button>
+    </div>
   )
 }
 
