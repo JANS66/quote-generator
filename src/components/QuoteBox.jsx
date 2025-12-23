7ca07981bd89cc91e2395e543c275e87ae3bbdd6
JANS66 <98973687+JANS66@users.noreply.github.com>
2025-12-23 15:50:06 +0200

    Create QuoteBox.jsx

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function QuoteBox(properties) {
+    return (
+        <div className="quote-box">
+            <p>"{properties.quote.content}"</p>
+            <p>- {properties.quote.author}</p>
+        </div>
+    )
+}
